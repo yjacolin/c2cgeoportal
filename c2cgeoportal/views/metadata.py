d202efccce5d346be16f666caf3013140ac9f86f
Stéphane Brunner <stephane.brunner@camptocamp.com>
2012-03-08 18:01:27 +0100

    fix metadata view with Tom

--- c2cgeoportal/views/metadata.py
+++ c2cgeoportal/views/metadata.py
@@ -1,6 +1,7 @@
 from papyrus.renderers import XSD
 from pyramid.httpexceptions import HTTPInternalServerError, HTTPNotFound
 from pyramid.view import view_config
+from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
 
 from c2cgeoportal.lib.dbreflection import get_class
 from c2cgeoportal.models import DBSession, Layer
@@ -10,10 +11,10 @@ from c2cgeoportal.models import DBSession, Layer
 def metadata(request):
     layer_id = int(request.matchdict.get('layer_id'))
     try:
-        table_name = DBSession.query(Layer.editTable).filter(Layer.id == layer_id).one()
-    except NoResultsFound:
+        table_name, = DBSession.query(Layer.geoTable).filter(Layer.id == layer_id).one()
+    except NoResultFound:
         return HTTPNotFound()
     except MultipleResultsFound:
         return HTTPInternalServerError()
     # FIXME add DBSession as argument to get_class to avoid deadlock
-    return get_class(table_name).__table__
+    return get_class(str(table_name)).__table__
