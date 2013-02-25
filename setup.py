4d5d9371217243c70c282c021fa20ba7b239db0f
Yves Jacolin <yves.jacolin@camptocamp.com>
2013-02-25 12:33:43 +0100

    set c2cgeoportal version to 1.2.1

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ tests_require = install_requires + [
     ]
 
 setup(name='c2cgeoportal',
-      version='1.2',
+      version='1.2.1',
       description='c2cgeoportal',
       long_description=README,
       classifiers=[
