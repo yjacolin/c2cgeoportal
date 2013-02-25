d37d365ff5cd30095a9021817b202567bbc5d020
Yves Jacolin <yves.jacolin@camptocamp.com>
2013-02-25 12:26:35 +0100

    fix versions for setup-requires dependencies so that buildout works again

--- setup.py
+++ setup.py
@@ -40,10 +40,10 @@ install_requires = [
 # tests_require, they need be in setup_requires
 setup_requires = [
     'nose==1.1.2',
-    'nosexcover',
-    'nose-progressive',
-    'ipdbplugin',
-    ]
+    'nosexcover==1.0.7',
+    'nose-progressive==1.3',
+    'ipdbplugin==1.2',
+]
 
 tests_require = install_requires + [
     'mock',
